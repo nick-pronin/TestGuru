59d2c557d9b9d7e5ad25ae5da8ae71b7924302cc
Master <vertupro061@gmail.com>
2021-07-24 17:03:29 +0300

    user and test methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+
+  def test_by_level(level)
+    Test.joins('INNER JOIN users_tests ON user_test.test_id = tests.id').where('tests.level = :level', level: level).where(users_tests: { user_id: self.id })
+  end
 end
