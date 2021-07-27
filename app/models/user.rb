7926fb794ce84bb3790be16ed30c2e8bc27f3d3e
Master <vertupro061@gmail.com>
2021-07-28 00:51:49 +0300

    fix mistakes again

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :results, dependent: :destroy
-  has_many :tests, through: :results, dependent: :destroy
+  has_many :tests, through: :results
   has_many :authorship, class_name: "Test", foreign_key: "author_id", dependent: :nullify
 
   def test_by_level(level)
