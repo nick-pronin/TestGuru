8e0e9e5290a6f8a140afee3a2f353954916ee1a5
Master <vertupro061@gmail.com>
2021-07-27 03:39:02 +0300

    added associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,6 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions, dependent: :destroy
 
   def self.sort_by_category(category)
     joins('INNER JOIN categories ON tests.category_id = categories.id').where(categories: { title: category }).order(id: :desc).pluck('tests.title')
