6c3aa76ce3c7bf538a9b5d39b55ee29326f72a7a
Master <vertupro061@gmail.com>
2021-07-29 09:08:29 +0300

    added index to tests

--- db/migrate/20210728144230_add_index_to_users_categories_tests.rb
+++ db/migrate/20210728144230_add_index_to_users_categories_tests.rb
@@ -1,6 +1,6 @@
 class AddIndexToUsersCategoriesTests < ActiveRecord::Migration[6.1]
   def change
-    add_index :tests, :title, unique: true
+    add_index :tests, [:title, :level], unique: true
     add_index :categories, :title, unique: true
     add_index :users, :email, unique: true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 2021_07_28_144230) do
     t.bigint "author_id"
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
-    t.index ["title"], name: "index_tests_on_title", unique: true
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
   end
 
   create_table "users", force: :cascade do |t|
